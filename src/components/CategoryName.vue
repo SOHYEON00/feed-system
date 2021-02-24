183801e0988d75fc1611342b95548d0a222b4470
SOHYEON00 <ohsohyeon24@gmail.com>
2021-02-24 21:14:43 +0900

    feat: title (...) 로 출력하기
    css > text-overflow 로 출력

---
+++ src/components/CategoryName.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hello!{{category_id}}</div>
+</template>
+
+<script>
+export default {
+    name: "CategoryName",
+    props: ['category_id'],
+    
+}
+
+</script>
+
+<style>
+
+</style>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <Filterbar />
-        <article v-for="(list, id) in lists" :key="id">
-           <section>
-               <span>{{list.category_id}}</span>
-               <span>{{id}}</span>
+        <article class="feed-article" v-for="(list, id) in lists" :key="id">
+           <section class="feed-header">
+               <span><CategoryName class="category" :category_id="list.category_id" /></span>
+               
+               <span class="feed-id">{{id}}</span>
             </section>
-            <section>
-                <span>{{list.user_id}}</span>
-                <span>{{list.created_at}}</span>
+            <section class="feed-contents">
+                <p class="feed-contents-head">
+                    <span class="user-id feed-contents-head">{{list.user_id}}</span>
+                    <span class="created feed-contents-head">{{list.created_at}}</span>  
+                </p>
+                <p class="title">{{list.title}}</p>
+                <!-- <p>{{list.contents}}</p> -->
             </section>
-            <p>{{list.title}}</p>
-            <p>{{list.contents}}</p>
         </article>
     </div>
 </template>
@@ -19,6 +22,7 @@
 <script>
 
 import Filterbar from './Filterbar';
+import CategoryName from './CategoryName';
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
@@ -31,7 +35,7 @@ export default {
     name: 'MainSection',
     components: {
         Filterbar,
-        
+        CategoryName
     },
     data() {
         return {
@@ -54,7 +58,6 @@ export default {
                 console.log(response.data.data);
                 const realData = response.data.data;
                 this.lists = realData;
-                console.log(typeof(realData));
             }
             
         })
@@ -68,5 +71,54 @@ export default {
 </script>
 
 <style scoped>
+    .feed-article {
+        background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        border: 1px solid #E1E4E7;
+        border-radius: 5px;
+        opacity: 1;
+        padding: 2rem;
+        margin: 2rem 0;
+    }
 
+    .feed-header {
+        display: flex;
+        justify-content: space-between;
+        text-align: left;
+        font: normal normal normal 13px/25px SpoqaHanSans;
+        letter-spacing: 0px;
+        color: #7E848A;
+        opacity: 1;
+        border-bottom: 1px solid #E1E4E7;
+        padding-bottom: 0.5rem;
+    }
+
+    .feed-contents {
+        display: block;
+        text-align: left;
+        font: normal normal normal 13px/25px SpoqaHanSans;
+        letter-spacing: 0px;
+        opacity: 1;
+    }
+
+    .feed-contents-head {
+        padding-right: 0.5rem;
+    }
+    .user-id {
+        color: var(--unnamed-color-00c854);
+    }
+
+    .created {
+        color: var(--unnamed-color-495057);
+    }
+
+    .title {
+        font: normal normal bold 18px/28px SpoqaHanSans;
+        color: #282C30;
+        text-overflow: ellipsis;
+        height: 3vh;
+        white-space: nowrap;
+        width: 100%;
+        overflow: hidden;
+    }
 </style>
