a8abe57d8ac4c5084de3a19f19a0906719ee2b32
SOHYEON00 <ohsohyeon24@gmail.com>
2021-02-24 11:49:37 +0900

    docs: api 문서 type 생성 및 api 변수 작성

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "https://problem.comento.kr";
+
+export const HEADER = {Accept: application/json };
+
+// apis
+export const LIST = `${BASE_URL}/api/list`;
+export const CATEGORY = `${BASE_URL}/api/category`;
+export const ADS = `${BASE_URL}/api/ads`;
+export const VIEW = `${BASE_URL}/api/view`;
